442f5302e600b2c94b21556603a1a4469bb419a3
David Miller <dlm27@mapc-0178.(none)>
2009-02-24 10:29:48 +0000

    Added plot of the horseshoe for the ramsay results doc.

---
+++ sc-writeup/figs/horseshoeplot.R
@@ -0,0 +1,27 @@
+# plot the horseshoe for the doc
+
+
+library(soap)
+
+## create a boundary...
+fsb <- list(fs.boundary())
+
+## Plot the true function...
+# This takes a while to plot but looks nicer
+m<-300;n<-150
+xm <- seq(-1,3.5,length=m);yn<-seq(-1,1,length=n)
+xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
+
+tru <- matrix(fs.test(xx,yy),m,n) ## truth
+
+# output to pdf
+pdf("ramsayhorseshoe.pdf",4,4)
+
+image(xm,yn,tru,col=heat.colors(100),xlab="",ylab="",
+      main="",axes=FALSE)
+lines(fsb[[1]],lwd=3)
+contour(xm,yn,tru,levels=seq(-5,5,by=.25),add=TRUE)
+
+dev.off()
+
+
