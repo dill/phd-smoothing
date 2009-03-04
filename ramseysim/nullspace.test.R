da001496bbbf6fdeed4e71c2e03d762e1ce2ff52
dill <dill@enderleigh.(none)>
2009-03-04 18:28:56 +0000

    Ready to analyse the linear data.

--- ramseysim/nullspace.test.R
+++ ramseysim/nullspace.test.R
@@ -16,15 +16,6 @@ xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
 ## truth
 tru <- fs.test(xx,yy) 
 
-# read in the predicition grid from matlab
-predback.real<-read.csv("./matlab/preal.csv",header=F)
-predback.imag<-read.csv("./matlab/pimag.csv",header=F)
-prediction.grid<-data.frame(v=predback.real[[1]],w=predback.imag[[1]])
-
-
-# bit of faffing with titles
-names(fsb[[1]]) <- c("x","y") ## correct boundary names
-insiders<-inSide(fsb,x=xx,y=yy)
 names(fsb[[1]]) <- c("v","w") ## correct boundary names
 
 
@@ -59,13 +50,13 @@ curve.y<-c(curve.y,-curve.y[length(curve.y):1])
 fs.centreline<-list(x=c(r.leg.x,curve.x,leg.x),y=c(leg.y.t,curve.y,leg.y.b))
 
 # test
-fsb <- fs.boundary()
-m<-300;n<-150 
-xm <- seq(-1,4,length=m);yn<-seq(-1,1,length=n)
-xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
-tru <- matrix(fs.test(xx,yy),m,n) ## truth
-image(xm,yn,tru,col=heat.colors(100),xlab="x",ylab="y")
-points(fs.centreline,pch=".")
+#fsb <- fs.boundary()
+#m<-300;n<-150 
+#xm <- seq(-1,4,length=m);yn<-seq(-1,1,length=n)
+#xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
+#tru <- matrix(fs.test(xx,yy),m,n) ## truth
+#image(xm,yn,tru,col=heat.colors(100),xlab="x",ylab="y")
+#points(fs.centreline,pch=".")
 
 
 # let evaluate these points using the fs.test function
@@ -82,49 +73,12 @@ fs.centre.eval<-fs.test(fs.centreline$x,fs.centreline$y)
 
 # this line is equiv. to...
 # export to matlab
-write.csv(fs.centreline,"centreline.csv",row.names=FALSE)
+#write.csv(fs.centreline,"centreline.csv",row.names=FALSE)
 
 
-# take those points, map them
-
-# evaluate the pointsusing fs.test
-
-
-# linear fit of x,y against f.
+# read the points in
+mapped.centreline<-read.csv("centrelinemapped.csv",header=F)
+names(mapped.centreline)<-c("x","y")
 
 
-
-
-
-#linear.fit<-lm(tru~prediction.grid$v*prediction.grid$w)
-linear.fit<-lm(tru~prediction.grid$v+prediction.grid$w)
-
-
-
-#pred<-predict(linear.fit,prediction.grid)
-#pred[!insiders]<-NA
-
-
-# predict with w set and vary v?
-# then the other way around?
-
-
-
-
-
-
-
-
-#put the predicted linear points z values onto the horseshoe
-
-
-
-
-image(matrix(pred,m,n),col=heat.colors(100))
-contour(matrix(pred,m,n),nlevels=50,add=TRUE)
-
-
-
-
-
-
+# now do some plotting of these...
