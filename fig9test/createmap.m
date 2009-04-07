4ddb2911698453a0ee97d3e1e2aadbea8518ac90
David Miller <dlm27@mapc-0178.(none)>
2009-04-07 12:01:08 +0100

    Made necessary modifications to maple code.

--- fig9test/createmap.m
+++ fig9test/createmap.m
@@ -6,20 +6,6 @@ path(path,'../matlab/poly_stuff')
 % read in the vertices
 polyv=csvread('figverts.csv',0,0)
 
-%polyv =
-%
-%    8.5921    0.3638
-%    8.8262    6.1035
-%    3.4040    6.2340
-%    2.8969    8.7995
-%   -3.6176    7.7559
-%   -2.8374    2.7554
-%   -9.3518    0.9291
-%   -6.2701   -6.2021
-%    5.9005   -7.6370
-%    8.5921    0.3638
-
-
 % create the polygon
 polyv_c=complex(polyv(:,1),polyv(:,2))
 polyv_p=polygon(complex(polyv(:,1),polyv(:,2)))
@@ -31,11 +17,15 @@ vertex_points=[1 6 8 9]
 % do the mapping
 f=rectmap(polyv_p, vertex_points)
 
-
-
 %%%%%%%%%%%%%%%%%%%
 % load the truth points
 sample_points=csvread('fig9truth.csv',1,0)
+
+% copy to output
+out_samp=sample_points
+
+% select the points we want 
+sample_points=sample_points(sample_points(:,4)==1,1:2)
 
 % make the data into a complex var
 sample_points=complex(sample_points(:,1),sample_points(:,2))
@@ -43,5 +33,5 @@ sample_points=complex(sample_points(:,1),sample_points(:,2))
 % map those points
 mapped_sample_points=evalinv(f,sample_points)
 
-csvwrite('fig9truemapped.csv',[real(mapped_sample_points),imag(mapped_sample_points)])
+csvwrite('fig9truemapped.csv',[real(mapped_sample_points),imag(mapped_sample_points),out_samp(:,3)])
 
