52ec86a277f3f2e1b8db3a084f0ee7d84aeb0ceb
David Miller <dlm27@mapc-0178.(none)>
2009-04-07 16:07:58 +0100

    Not looking so good...

--- fig9test/createmap.m
+++ fig9test/createmap.m
@@ -15,7 +15,13 @@ polyv_p=polygon(complex(polyv(:,1),polyv(:,2)))
 vertex_points=[1 6 8 9]
 
 % do the mapping
+
+% rectangle map
 f=rectmap(polyv_p, vertex_points)
+
+% disk map
+%f=crdiskmap(polyv_p)
+
 
 %%%%%%%%%%%%%%%%%%%
 % load the truth points
--- fig9test/fit.irregular.R
+++ fig9test/fit.irregular.R
@@ -1,23 +1,26 @@
 # Fit a gam to the irregular region once it's been mapped
 
-
 # load some data...
 true.vals<-read.csv("fig9truth.csv",header=TRUE)
 true.vals.mapped<-read.csv("fig9truemapped.csv",header=FALSE)
-names(true.vals.mapped)<-c("x","y")
+names(true.vals.mapped)<-c("x","y","z")
 
 
 # sample from the matrix
 
 # how many points to sample
-samp.size<-1000
+samp.size<-10000
 
 # make a sample index
 this.sample<-sample(c(1:dim(true.vals)[1]),samp.size)
 
+
+# noise
+ran<-rnorm(samp.size)*0.02
+
 # take the points from the true and true mapped
-samp.data<-data.frame(x=true.vals$x[this.sample],y=true.vals$y[this.sample],z=true.vals$z[this.sample])
-samp.data.mapped<-data.frame(x=true.vals.mapped$x[this.sample],y=true.vals.mapped$y[this.sample],z=true.vals$z[this.sample])
+samp.data<-data.frame(x=true.vals$x[this.sample],y=true.vals$y[this.sample],z=true.vals$z[this.sample]+ran)
+samp.data.mapped<-data.frame(x=true.vals.mapped$x[this.sample],y=true.vals.mapped$y[this.sample],z=true.vals.mapped$z[this.sample]+ran)
 
 
 # run mgcv
@@ -31,30 +34,41 @@ fv <- predict(b.mapped,newdata=data.frame(x=true.vals.mapped$x,y=true.vals.mappe
 
 
 
+res<-sqrt(length(true.vals$x))
 par(mfrow=c(2,2))
 
 #surf[!inside.points]<-NA
 
 ### first truth
-image(surf,col=heat.colors(100),xlab="v",ylab="w",main="truth")
-contour(surf,add=T)
+tru<-matrix(c(0),res,res)
+tru[true.vals$inside==1]<-true.vals$z[true.vals$inside==1]
+tru[true.vals$inside==0]<-NA
+image(tru,col=heat.colors(100),xlab="v",ylab="w",main="truth")
+contour(tru,add=T)
 
 ### sc prediction w. tprs 
 # do some faffing for the plots
-pred.grid<-matrix(c(0),1000,1000)
-pred.grid[inside.points]<-fv
-pred.grid[!inside.points]<-NA
+pred.grid<-matrix(c(0),res,res)
+pred.grid[true.vals$inside==1]<-fv
+pred.grid[true.vals$inside==0]<-NA
 
 image(pred.grid,col=heat.colors(100),xlab="v",ylab="w",main="sc+tprs prediction")
 contour(pred.grid,add=T)
 
 ### normal tprs
 b.tprs<-gam(z~s(x)+s(y),data=samp.data)
-fv.tprs <- predict(b.tprs,newdata=data.frame(x=true.vals$x,y=true.vals$y))
+fv.tprs <- predict(b.tprs,newdata=data.frame(x=true.vals$x[true.vals$inside==1],y=true.vals$y[true.vals$inside==1]))
 
-pred.grid.tprs<-matrix(c(0),1000,1000)
-pred.grid.tprs[inside.points]<-fv.tprs
-pred.grid.tprs[!inside.points]<-NA
+pred.grid.tprs<-matrix(c(0),res,res)
+pred.grid.tprs[true.vals$inside==1]<-fv.tprs
+pred.grid.tprs[true.vals$inside==0]<-NA
 image(pred.grid.tprs,col=heat.colors(100),xlab="v",ylab="w",main="tprs prediction")
 contour(pred.grid.tprs,add=T)
+
+
+
+### calculate the MSEs
+
+cat("sc+tprs",mean((true.vals$z[true.vals$inside==1]-fv)^2,na.rm=T),"\n")
+cat("tprs",mean((true.vals$z[true.vals$inside==1]-fv.tprs)^2,na.rm=T),"\n")
 
