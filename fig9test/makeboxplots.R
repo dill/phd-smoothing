05213b086f41fd18b9a3f4444ecc271dfad640f3
David Miller <dlm27@mapc-0178.(none)>
2009-04-23 17:28:58 +0100

    Still waiting for simulations.

--- fig9test/makeboxplots.R
+++ fig9test/makeboxplots.R
@@ -1,12 +1,12 @@
 # make some boxplots
 # plots on log scale, MSES reported underneath are _not_
 
+
+pdf(file=paste("mses-",domain,"-boxplot.pdf",sep=""),width=6,height=10)
 par(mfrow=c(4,1))
 
 # make the text look better for printout
 par(cex.lab=0.5)
-
-pdf(file=paste("mses-",domain,"-boxplot.pdf",sep=""),width=6,height=10)
 
 for (samp.size in c(1000,500)){
    for(noise.level in c(0.02, 0.005)){
