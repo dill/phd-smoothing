93fed667a965c4484a2e470b1530b8191a448e3f
David Lawrence Miller <dill@kiff.local>
2009-08-15 11:55:10 +0100

    O2 gets rid of loop error. Path still not there...

--- mds/make.sh
+++ mds/make.sh
@@ -4,11 +4,11 @@
 #gcc -lm -g test.c inout.c wood.c
 
 # w-shaped area
-gcc -lm -g doubleyuh-test.c inout.c wood.c 
+#gcc -lm -g doubleyuh-test.c inout.c wood.c 
 
 # linked list test
 #gcc -lm -g ll-test.c inout.c 
 
 # shared object file
-#gcc -lm -g -std=gnu99 -shared inout.c wood.c -o wood.so -L/usr/lib/R/lib  -fPIC #-lR
+gcc -lm -g -std=gnu99 -shared inout.c wood.c -o wood.so -L/usr/lib/R/lib  -fPIC -O2 #-lR
 #rm *.o
