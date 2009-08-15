61798b4b4e3848ffeadf596b49676ec091cda1df
David Miller <dlm27@talent.bath.ac.uk>
2009-08-15 09:51:53 +0100

    ...

--- mds/make.sh
+++ mds/make.sh
@@ -4,11 +4,11 @@
 #gcc -lm -g test.c inout.c wood.c
 
 # w-shaped area
-gcc -lm -g doubleyuh-test.c inout.c wood.c 
+#gcc -lm -g doubleyuh-test.c inout.c wood.c 
 
 # linked list test
 #gcc -lm -g ll-test.c inout.c 
 
 # shared object file
-#gcc-4.2 -lm -g -std=gnu99 -shared inout.c wood.c -o wood.so -L/usr/lib/R/lib  -fPIC #-lR
+gcc -lm -g -std=gnu99 -shared inout.c wood.c -o wood.so -L/usr/lib/R/lib  -fPIC #-lR
 #rm *.o
