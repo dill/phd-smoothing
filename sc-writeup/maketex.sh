a95431afb3da6e8101396775ca81f7a462f0ab2e
David Miller <dlm27@mapc-0178.maths.bath.ac.uk>
2009-10-02 08:57:54 +0100

    ...

--- sc-writeup/maketex.sh
+++ sc-writeup/maketex.sh
@@ -1,5 +1,19 @@
-# make the file...
+#  schwarzchristoffel.tex
 pdflatex schwarzchristoffel.tex
 bibtex schwarzchristoffel
 pdflatex schwarzchristoffel.tex
 pdflatex schwarzchristoffel.tex
+
+
+# ramsay results
+pdflatex ramsayresults.tex
+bibtex ramsayresults
+pdflatex ramsayresults.tex
+pdflatex ramsayresults.tex
+
+
+# otherdomains
+pdflatex otherdomains.tex
+bibtex otherdomains
+pdflatex otherdomains.tex
+pdflatex otherdomains.tex
