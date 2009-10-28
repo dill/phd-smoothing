fe6077713c17439e61046da3b4578d5a35fb44a2
David Lawrence Miller <dill@kiff.local>
2009-10-28 11:13:12 +0000

    Change of transform...

--- mds/intexp/intexp.R
+++ mds/intexp/intexp.R
@@ -55,7 +55,7 @@ xk<-1:7/8 #choosesomeknots
 mod.2<-prs.fit(wear,x,xk,0.0001)# fitpen.reg.spline 
 Xp<-spl.X(xp,xk)#matrixtomapparamstofittedvaluesatxp 
 
-par(mfrow=c(3,1))
+par(mfrow=c(2,2))
 #plot data & spl.fit 
 plot(x,wear, main="normal fit")
 lines(xp,Xp%*%coef(mod.2))
@@ -71,24 +71,36 @@ rug(x,lwd=2)
 # move around the values of x and xk
 xk<-1:7/8 #choose some knots 
 xp<-seq(0,1.2,len=120) # xvaluesforprediction 
+
+
+# don't think that this works since the knots are at the folds
+#xk.m<-xk
+#x.m[x.m>xk.m[3]]<-x.m[x.m>xk.m[3]]+0.2
+#xp.m<-xp
+#xp.m[xp.m>xk.m[3]]<-xp.m[xp.m>xk.m[3]]+0.2
+#xk.m[xk.m>xk.m[3]]<-xk.m[xk.m>xk.m[3]]+0.2
+#x.m[x.m>xk.m[4] & x.m<xk.m[7]]<-x.m[x.m>xk.m[4] & x.m<xk.m[7]]-0.1
+#xp.m[xp.m>xk.m[4] & xp.m<xk.m[7]]<-xp.m[xp.m>xk.m[4] & xp.m<xk.m[7]]-0.1
+#xk.m[xk.m>xk.m[4] & xk.m<xk.m[7]]<-xk.m[xk.m>xk.m[4] & xk.m<xk.m[7]]-0.2
+
+
+# simpler
 x.m<-x
-
-xk.m<-xk
-x.m[x.m>xk.m[3]]<-x.m[x.m>xk.m[3]]+0.2
 xp.m<-xp
-xp.m[xp.m>xk.m[3]]<-xp.m[xp.m>xk.m[3]]+0.2
-xk.m[xk.m>xk.m[3]]<-xk.m[xk.m>xk.m[3]]+0.2
-x.m[x.m>xk.m[4] & x.m<xk.m[7]]<-x.m[x.m>xk.m[4] & x.m<xk.m[7]]-0.1
-xp.m[xp.m>xk.m[4] & xp.m<xk.m[7]]<-xp.m[xp.m>xk.m[4] & xp.m<xk.m[7]]-0.1
-xk.m[xk.m>xk.m[4] & xk.m<xk.m[7]]<-xk.m[xk.m>xk.m[4] & xk.m<xk.m[7]]-0.2
-
+xk.m<-xk
+x.m[x.m>0.3 & x.m<0.7]<-x.m[x.m>0.3 & x.m<0.7]*0.8
+xk.m[xk.m>0.3 & xk.m<0.7]<-xk.m[xk.m>0.3 & xk.m<0.7]*0.8
+xp.m[xp.m>0.3 & xp.m<0.7]<-xp.m[xp.m>0.3 & xp.m<0.7]*0.8
+x.m[x.m>0.7]<-x.m[x.m>0.7]-0.24
+xk.m[xk.m>0.7]<-xk.m[xk.m>0.7]-0.24
+xp.m[xp.m>0.7]<-xp.m[xp.m>0.7]-0.24
 
 mod.2<-prs.fit(wear,x.m,xk.m,0.0001)# fitpen.reg.spline 
 Xp.move<-spl.X(xp.m,xk.m)#matrix to map params to fittedv alues at xp 
 
 
 #plot data & spl.fit 
-plot(x.m,wear, main="squash fit")
+plot(x,wear, main="squash fit")
 lines(xp,Xp.move%*%coef(mod.2))
 abline(v=xk.m,col="green",lwd=2)
 rug(x,lwd=2)
@@ -111,8 +123,8 @@ intR<-function(xk1,xk2,xk,xk.m,max.x){
    intrange<-c(0,xk.m,max.x)
 
    # find the weights
-   #w<-1/abs(diff(intrange)/diff(c(0,1:7/8,1)))
-   w<-rep(1,length(intrange))
+   w<-abs(diff(intrange)/diff(c(0,1:7/8,1)))^4
+   #w<-rep(1,length(intrange))
 
    #cat("w=",w,"\n")
 
@@ -151,26 +163,50 @@ spl.S<-function(xk,xk.m,max.x){
 } 
 
 
-prs.fit<-function(y,x,xk.m,xk,lambda) {
-   q<-length(xk)+2 #dimension of basis 
+prs.fit<-function(y,x,xk,xk.m,lambda) {
+   q<-length(xk.m)+2 #dimension of basis 
    n<-length(x) #number of data 
    # create augmented model matrix.... 
-   Xa<-rbind(spl.X(x,xk),mat.sqrt(spl.S(xk,xk.m,max(x)))*sqrt(lambda)) 
+   Xa<-rbind(spl.X(x,xk.m),mat.sqrt(spl.S(xk,xk.m,max(x)))*sqrt(lambda)) 
    y[(n+1):(n+q)]<-0 #augment the data vector 
    lm(y~Xa-1)#fit and return penalized regression spline 
 } 
 
-#library(debug)
-#mtrace(intR)
-#mtrace(spl.S)
 mod.2<-prs.fit(wear,x.m,xk,xk.m,0.0001)# fitpen.reg.spline 
 Xp.move<-spl.X(xp.m,xk.m)#matrix to map params to fittedv alues at xp 
 
-modS<-spl.S(xk,xk.m,max(x))
+modS<-spl.S(xk.m,xk,max(x.m))
 
 #plot data & spl.fit 
-plot(x.m,wear, main="fixed fit?")
+plot(x,wear, main="fixed fit?")
 lines(xp,Xp.move%*%coef(mod.2))
 abline(v=xk.m,col="green",lwd=2)
 rug(x,lwd=2)
+
+
+
+
+
+mod.2<-prs.fit(wear,x,xk,xk,0.0001)# fitpen.reg.spline 
+Xp.move<-spl.X(xp,xk)#matrix to map params to fittedv alues at xp 
+
+#plot data & spl.fit 
+plot(x,wear, main="old fit, new S estimation")
+lines(xp,Xp.move%*%coef(mod.2))
+abline(v=xk,col="green",lwd=2)
+rug(x,lwd=2)
+
+
+
+
+
+
+### dia
+#plot(x,wear)
+#abline(v=0.3,col="green",lwd=3)
+#abline(v=0.7,col="green",lwd=3)
+#points(x[x>0.3 & x<0.7]*0.8,y=wear[x>0.3 & x<0.7],pch=19,col="red")
+#abline(v=0.3*0.8,col="red",lwd=3)
+#abline(v=0.7*.8,col="red",lwd=3)
+#points(x[x>0.7]-.24,y=wear[x>0.7],pch=19,col="red")
 
