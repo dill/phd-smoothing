5c4a3977a05499673f3307111cae62f1a083a2fe
David Lawrence Miller <dill@kiff.local>
2010-01-20 20:27:02 +0000

    changed T shape example to look nicer...

--- mds-writeup/figs/gridtest.R
+++ mds-writeup/figs/gridtest.R
@@ -8,8 +8,8 @@ par(mfrow=c(2,2),cex=0.4,pch=19)
 
 
 # points for the polygon, sideways T shape
-pts.x<-c(0,0,0.2,0.2,0.8,0.8,0.2,0.2,0)
-pts.y<-c(0,0.5,0.5,0.3,0.3,0.2,0.2,0,0)
+pts.x<-c(0,0.5,0.5,0.3,0.3,0.2,0.2,0,0)
+pts.y<-c(0.8,0.8,0.6,0.6,0,0,0.6,0.6,0.8)
 bnd<-list(x=pts.x,y=pts.y)
 
 # plot it
@@ -17,7 +17,7 @@ plot(bnd,type="l",xlab="",ylab="")
 
 # grid across the whole shape
 m<-25;n<-25
-xm <- seq(0,0.8,length=m);yn<-seq(0,0.5,length=n)
+xm <- seq(0,0.5,length=m);yn<-seq(0,0.8,length=n)
 xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
 onoff<-inSide(bnd,xx,yy)
 onoff<-onoff&inSide(list(x=-bnd$x,y=-bnd$y),-xx,-yy)
@@ -35,13 +35,13 @@ plot(mds.full$points,xlab="",ylab="")
 
 
 # now MDS the "tail"
-D.tail<-create_distance_matrix(samp$x[samp$x>0.2],samp$y[samp$x>0.2],bnd)
+D.tail<-create_distance_matrix(samp$x[samp$y>0.6],samp$y[samp$y>0.6],bnd)
 mds.tail<-cmdscale(D.tail,k=2,eig=TRUE,x.ret=TRUE)
 
-plot(mds.tail$points,xlim=c(-0.55,0.3),asp=1,xlab="",ylab="")
+plot(mds.tail$points,xlim=c(-0.25,0.25),ylim=c(-0.55,0.1),asp=1,xlab="",ylab="")
 # insert the remaining points
-ins.tail<-insert.mds(list(x=samp$x[samp$x<=0.2],y=samp$y[samp$x<=0.2]),
-                     list(x=samp$x[samp$x>0.2],y=samp$y[samp$x>0.2]),
+ins.tail<-insert.mds(list(x=samp$x[samp$y<=0.6],y=samp$y[samp$y<=0.6]),
+                     list(x=samp$x[samp$y>0.6],y=samp$y[samp$y>0.6]),
                      mds.tail,bnd)
 points(ins.tail,col="red")
 
@@ -49,13 +49,13 @@ points(ins.tail,col="red")
 
 
 # now MDS the "head"
-D.head<-create_distance_matrix(samp$x[samp$x<=0.2],samp$y[samp$x<=0.2],bnd)
+D.head<-create_distance_matrix(samp$x[samp$y<=0.6],samp$y[samp$y<=0.6],bnd)
 mds.head<-cmdscale(D.head,k=2,eig=TRUE,x.ret=TRUE)
 
-plot(mds.head$points,ylim=c(-0.6,0.1),asp=1,xlab="",ylab="")
+plot(mds.head$points,ylim=c(-0.1,0.1),xlim=c(-0.3,0.55),asp=1,xlab="",ylab="")
 # insert the remaining points
-ins.head<-insert.mds(list(x=samp$x[samp$x>0.2],y=samp$y[samp$x>0.2]),
-                     list(x=samp$x[samp$x<=0.2],y=samp$y[samp$x<=0.2]),
+ins.head<-insert.mds(list(x=samp$x[samp$y>0.6],y=samp$y[samp$y>0.6]),
+                     list(x=samp$x[samp$y<=0.6],y=samp$y[samp$y<=0.6]),
                      mds.head,bnd)
 points(ins.head,col="red")
 
