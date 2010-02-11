391696d259f457507e3c5dd8b3aa930b1ad03c2b
David Lawrence Miller <dill@kiff.local>
2010-02-11 23:15:51 +0000

    sprial fail code

---
+++ mds/spiralfail.R
@@ -0,0 +1,14 @@
+
+source("mds.R")
+source("spiral/make_spiral.R")
+
+
+
+spir.dat<-make_spiral(n.grid=50)
+bnd<-spir.dat$bnd
+
+
+tst<-list(x=c(3.737994,4.196905),
+          y=c(6.204425,6.204425))
+D.grid<-create_distance_matrix(tst$x,tst$y,bnd)
+
