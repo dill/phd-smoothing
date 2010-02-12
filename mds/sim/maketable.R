4920715c05b07634e0154668761387425de3e992
David Lawrence Miller <dill@kiff.local>
2010-02-12 11:00:00 +0000

    additions to the report...
    EDF
    
    EDF results for wt2

--- mds/sim/maketable.R
+++ mds/sim/maketable.R
@@ -34,13 +34,16 @@ for(errlev in errlevs){
 cat("\n\n")
 
 #wt2
-basefilename<-"wt2-250-"
+basefilename.mse<-"wt2-mse-250-"
+basefilename.edf<-"wt2-edf-250-"
 errlevs<-c(0.05,0.5,5)
 
-cat(" & MDS & MDS (tensor) & soap & thin plate\\\\ \n")
+cat(" & & MSE & &  & EDF & \\\\ \n")
+cat(" & MDS & MDS (tensor) & soap & thin plate & MDS & MDS (tensor) & soap & thin plate\\\\ \n")
+
 for(errlev in errlevs){
 
-   dat<-read.csv(paste(basefilename,errlev,".csv",sep=""))
+   dat<-read.csv(paste(basefilename.mse,errlev,".csv",sep=""))
 
    # here I remove the two simulations where the PDE solution grid and knots
    # line up and cause soap to fail
@@ -48,10 +51,19 @@ for(errlev in errlevs){
    mses<-c(mean(dat$mds),mean(dat$mdstp),mean(dat$soap[dat$soap<10]),mean(dat$tprs))
    ses<-c(sd(dat$mds),sd(dat$mdstp),sd(dat$soap[dat$soap<10]),sd(dat$tprs))/sqrtn
 
+   edf.dat<-read.csv(paste(basefilename.edf,errlev,".csv",sep=""))
+   edfs<-c(mean(edf.dat$mds),mean(edf.dat$mdstp),mean(edf.dat$soap),mean(edf.dat$tprs))
+   edfse<-c(sd(edf.dat$mds),sd(edf.dat$mdstp),sd(edf.dat$soap),sd(edf.dat$tprs))/sqrtn
+
+
    cat(errlev," & ")
-   cat(round(mses[1],4)," (",round(ses[1],4),") & ",sep="")
-   cat(round(mses[2],4)," (",round(ses[2],4),") & ",sep="")
-   cat(round(mses[3],4)," (",round(ses[3],4),") & ",sep="")
-   cat(round(mses[4],4)," (",round(ses[4],4),")\\\\ \n",sep="")
+   cat(round(mses[1],4)," (",round(ses[1],5),") & ",sep="")
+   cat(round(mses[2],4)," (",round(ses[2],5),") & ",sep="")
+   cat(round(mses[3],4)," (",round(ses[3],5),") &",sep="")
+   cat(round(mses[4],4)," (",round(ses[3],5),") &",sep="")
+   cat(round(edfs[1],4)," (",round(edfse[1],5),") & ",sep="")
+   cat(round(edfs[2],4)," (",round(edfse[2],5),") & ",sep="")
+   cat(round(edfs[3],4)," (",round(edfse[3],5),") & ",sep="")
+   cat(round(edfs[4],4)," (",round(edfse[3],5),")\\\\ \n",sep="")
 
 }
