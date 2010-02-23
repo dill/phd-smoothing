83edae13bfa6fd1d0fdbf23ae10ad7506957c981
David Lawrence Miller <dill@mapc-0178.campus.bath.ac.uk>
2010-02-23 13:06:37 +0000

    new spiral fail...

--- mds/spiralfail.R
+++ mds/spiralfail.R
@@ -7,9 +7,14 @@ source("spiral/make_spiral.R")
 spir.dat<-make_spiral(50,n.grid=10)
 bnd<-spir.dat$bnd
 
+tst<-list()
+tst$x<-c(4.295244,3.115185)
+tst$y<-c(3.455492,5.123384)
 
-tst<-list(x=c(3.737994,4.196905),
-          y=c(6.204425,6.204425))
+
+
+
+
 D.grid<-create_distance_matrix(tst$x,tst$y,bnd)
 
 options(echo=TRUE)
