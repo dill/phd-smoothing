ebe0dfe9ea71ef882066fa97f18a85e15326fad4
David Lawrence Miller <dill@mapc-0178.campus.bath.ac.uk>
2010-03-05 14:00:12 +0000

    Error in eval(expr, envir, enclos) :   non-positive values not allowed for the gamma family
    fix!

--- mds/aral/sim.R
+++ mds/aral/sim.R
@@ -34,122 +34,74 @@ bnd<-list(x=bnd.km$km.e,y=bnd.km$km.n)
 ### First fit the soap
 
 # create prediction grid for soap
-s.grid<-make_soap_grid(bnd,c(10,10))
+s.grid<-make_soap_grid(bnd,10)
 
 s.grid<-pe(s.grid,-2)
 
-b.soap<-gam(chl~s(x,y,k=49,bs="so",xt=list(bnd=list(bnd))),knots=s.grid,
+b.soap<-gam(chl~s(x,y,k=80,bs="so",xt=list(bnd=list(bnd))),knots=s.grid,
             family=Gamma(link="log"),data=aral.dat)
 
 # now predict over the domain, this is now "truth"
+pred.n<-80 # prediction grid size
+
+# create the prediction points
+pred.points<-make_soap_grid(bnd,pred.n)
+
+# make the soap prediction
+new.truth<-predict(b.soap,newdata=pred.points)
+
+# plots
+#pred.mat<-make_soap_grid(bnd,pred.n,mat=T)$mat
+#pred.mat[!is.na(pred.mat)]<-new.truth
+#image(pred.mat)
 
 
+# pre-calculate the MDS base configuration
+mds.grid<-make_soap_grid(bnd,15)
+D<-create_distance_matrix(mds.grid$x,mds.grid$y,bnd)
+grid.mds<-cmdscale(D,eig=TRUE,k=2,x.ret=TRUE)
+
+n.sim<-1
+n.samp<-100
+noise.level<-0.5
+
+mses<-matrix(0,n.sim,3)
+
+for(i in 1:n.sim){
+
+   ## take the sample and add some noise
+   samp.ind<-sample(1:length(new.truth),n.samp)
+   noise<-rnorm(n.samp)*noise.level
+   samp<-data.frame(x=pred.points$x[samp.ind],
+                    y=pred.points$y[samp.ind],
+                    chl=new.truth[samp.ind]+noise)
+
+  
+   ### fit some models
+   # tprs
+   tp.fit<-gam(chl~s(x,y,k=49),data=samp,family=Gamma(link="log"))
+
+   # soap
+   soap.fit<-gam(chl~s(x,y,k=80,bs="so",xt=list(bnd=list(bnd))),knots=s.grid,
+            family=Gamma(link="log"),data=samp)
+
+   # MDS
+   samp.mds<-insert.mds(samp,mds.grid,grid.mds,bnd)
+   samp.mds$chl<-samp$chl[samp.ind]
+   mds.fit<-gam(chl~s(x,y,k=49),data=samp.mds,family=Gamma(link="log"))
 
 
+   ### do some prediction
+   tp.pred<-predict(tp.fit,newdata=new.truth)
+   soap.pred<-predict(soap.fit,newdata=new.truth)
 
-#tp.fit<-gam(chl~s(x,y,k=49),data=aral.dat)
-#
-## prediction grid
-#m<-50;n<-50
-#xm <- seq(min(aral.dat$x),max(aral.dat$x),length=m)
-#yn<-seq(min(aral.dat$y),max(aral.dat$y),length=n)
-#xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
-#pred.onoff<-inSide(bnd,xx,yy)
-#pred.grid<-data.frame(x=xx[pred.onoff],y=yy[pred.onoff])
-#
-#tp.pred<-predict(tp.fit,newdata=pred.grid)
-#pred.mat<-matrix(NA,m,n)
-#pred.mat[pred.onoff]<-tp.pred
-#image(pred.mat,x=unique(xx),y=unique(yy),main="tprs")
+   pred.mds<-insert.mds(new.truth,mds.grid,grid.mds,bnd)
+   mds.pred<-predict(mds.fit,newdata=pred.mds)
 
 
+   # calculate the MSE
+   mses[i,1]<-mean((tp.pred.mapped-new.truth)^2,na.rm=T)
+   mses[i,2]<-mean((soap.pred-new.truth)^2,na.rm=T)
+   mses[i,3]<-mean((mds.pred-new.truth)^2,na.rm=T)
 
-
-
-
-
-
-
-
-###### OTHER MODELS
-
-#### fit a thin plate model
-#tp.fit<-gam(chl~s(x,y,k=49),data=aral.dat)
-#
-## prediction grid
-#m<-50;n<-50
-#xm <- seq(min(aral.dat$x),max(aral.dat$x),length=m)
-#yn<-seq(min(aral.dat$y),max(aral.dat$y),length=n)
-#xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
-#pred.onoff<-inSide(bnd,xx,yy)
-#pred.grid<-data.frame(x=xx[pred.onoff],y=yy[pred.onoff])
-#
-#tp.pred<-predict(tp.fit,newdata=pred.grid)
-#pred.mat<-matrix(NA,m,n)
-#pred.mat[pred.onoff]<-tp.pred
-#image(pred.mat,x=unique(xx),y=unique(yy),main="tprs",xlab="km (East)",ylab="km (North)")
-#
-##### MDS
-## mds grid
-#m<-20;n<-20
-#xm <- seq(min(aral.dat$x),max(aral.dat$x),length=m)
-#yn<-seq(min(aral.dat$y),max(aral.dat$y),length=n)
-#xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
-#grid.onoff<-inSide(bnd,xx,yy)
-#mds.grid<-data.frame(x=xx[grid.onoff],y=yy[grid.onoff])
-#
-## actually do the MDS
-#D<-create_distance_matrix(mds.grid$x,mds.grid$y,bnd)
-#grid.mds<-cmdscale(D,eig=TRUE,k=2,x.ret=TRUE)
-#
-## create the data frame and fit the model
-#aral.mds<-insert.mds(aral.dat,mds.grid,grid.mds,bnd)
-#aral.mds<-data.frame(x=aral.mds[,1],
-#                     y=aral.mds[,2],
-#                     chl=aral.dat$chl)
-#
-#
-#mds.fit<-gam(chl~s(x,y,k=49),data=aral.mds)
-#
-## prediction grid
-#m<-50;n<-50
-#xm <- seq(min(aral.dat$x),max(aral.dat$x),length=m)
-#yn<-seq(min(aral.dat$y),max(aral.dat$y),length=n)
-#xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
-#pred.onoff<-inSide(bnd,xx,yy)
-#pred.grid<-data.frame(x=xx[pred.onoff],y=yy[pred.onoff])
-#pred.grid.mds<-insert.mds(pred.grid,mds.grid,grid.mds,bnd)
-#
-#pred.grid.mds<-data.frame(x=pred.grid.mds[,1],
-#                          y=pred.grid.mds[,2])
-#mds.pred<-predict(mds.fit,newdata=pred.grid.mds)
-#pred.mat<-matrix(NA,m,n)
-#pred.mat[pred.onoff]<-mds.pred
-#image(pred.mat,x=unique(xx),y=unique(yy),main="mds",xlab="km (East)",ylab="km (North)")
-
-#### soap 
-#tp.fit<-gam(chl~s(x,y,k=49),data=aral.dat)
-#
-## prediction grid
-#m<-50;n<-50
-#xm <- seq(min(aral.dat$x),max(aral.dat$x),length=m)
-#yn<-seq(min(aral.dat$y),max(aral.dat$y),length=n)
-#xx <- rep(xm,n);yy<-rep(yn,rep(m,n))
-#pred.onoff<-inSide(bnd,xx,yy)
-#pred.grid<-data.frame(x=xx[pred.onoff],y=yy[pred.onoff])
-#
-#tp.pred<-predict(tp.fit,newdata=pred.grid)
-#pred.mat<-matrix(NA,m,n)
-#pred.mat[pred.onoff]<-tp.pred
-#image(pred.mat,x=unique(xx),y=unique(yy),main="tprs")
-
-
-
-
-
-
-
-
-
-
-
+}
