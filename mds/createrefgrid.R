d3d637dc91433312b09a03b08c6360be2a5158bc
David Lawrence Miller <dill@mapc-0178.campus.bath.ac.uk>
2010-03-19 17:13:05 +0000

    now sensitive to start grid

--- mds/createrefgrid.R
+++ mds/createrefgrid.R
@@ -2,23 +2,23 @@ create_refgrid<-function(bnd){
    # create reference grid for the path finding
 
    # using a square grid: make_soap_grid   
-#   grid<-make_soap_grid(bnd,8)
+   grid<-make_soap_grid(bnd,10)
 
 
    # using triangulation
 
-   library(ads)
-
-   # first need to get rid of duplicate verts
-   bndtmp<-pe(bnd,-1)
-
-   bnd.tri<-triangulate(bndtmp)
-
-
-   # centres:
-   grid<-list(x=c(),y=c())
-   grid$x<-(1/3)*rowSums(bnd.tri[,c(1,3,5)])
-   grid$y<-(1/3)*rowSums(bnd.tri[,c(2,4,6)])
+#   library(ads)
+#
+#   # first need to get rid of duplicate verts
+#   bndtmp<-pe(bnd,-1)
+#
+#   bnd.tri<-triangulate(bndtmp)
+#
+#
+#   # centres:
+#   grid<-list(x=c(),y=c())
+#   grid$x<-(1/3)*rowSums(bnd.tri[,c(1,3,5)])
+#   grid$y<-(1/3)*rowSums(bnd.tri[,c(2,4,6)])
 
 
 
