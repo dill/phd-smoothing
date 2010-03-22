b975414b2cbb21425b1748f4eb8aff95791dc9b3
David Lawrence Miller <dill@mapc-0178.campus.bath.ac.uk>
2010-03-22 10:58:28 +0000

    added derivative code for tprs modifications...

---
+++ mds/intexp/derivs.R
@@ -0,0 +1,50 @@
+# derivatives for tprs
+# assume here that the penalty order is 2! (ie, m=2)
+
+# just eta in 1D
+eta1<-function(x,xk){
+   gamma(-1.5)/(16*sqrt(pi))*sqrt((x-xk)^3)
+}
+
+# first derivative
+deta1<-function(x,xk){
+   3*gamma(-1.5)/(16*sqrt(pi))*(1-xk)*(x-xk)^2
+}
+
+# second derivative
+ddeta1<-function(x,xk){
+   6*gamma(-1.5)/(16*sqrt(pi))*(1-xk)^2*(x-xk)
+}
+
+# eta in 2D
+deta2x1<-function(x,xk){
+   ss<-sum((x-xk)^2)
+   (1/(8*pi))*ss*log(sqrt(ss))
+}
+
+# 1st deriv eta in 2D, x1
+deta2x1<-function(x,xk){
+   ss<-sum((x-xk)^2)
+   (1/(8*pi))*(x1-xk[1])*(1+log(ss))
+}
+
+# 1st deriv eta in 2D, x2
+deta2x2<-function(x,xk){
+   ss<-sum((x-xk)^2)
+   (1/(8*pi))*(x2-xk[2])*(1+log(ss))
+}
+
+# second derivative x1^2
+ddeta2x1<-function(x1,x2,xk){
+   ss<-sum((x-xk)^2)
+   (1/(8*pi))*(1 + log(sqrt(ss)) + 2*((x1-xk[1])^2)/(ss))
+}
+
+# second derivative x2^2
+ddeta2x1<-function(x1,x2,xk){
+   ss<-sum((x-xk)^2)
+   (1/(8*pi))*(1 + log(sqrt(ss)) + 2*((x2-xk[2])^2)/(ss))
+}
+
+
+
