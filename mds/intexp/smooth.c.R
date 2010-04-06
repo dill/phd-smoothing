77d9e409b5ca4b0f71837023c92d3414bdba9636
David Lawrence Miller <dill@kiff.local>
2010-04-06 10:35:53 +0100

    going to start 2d....

--- mds/intexp/smooth.c.R
+++ mds/intexp/smooth.c.R
@@ -3,7 +3,7 @@
 
 ## The constructor for a tprs basis object with MDS modifications.
 smooth.construct.mdstp.smooth.spec<-function(object,data,knots){
- 
+
    # make the tprs object as usual
    object<-smooth.construct.tp.smooth.spec(object,data,knots)
 
@@ -13,8 +13,8 @@ smooth.construct.mdstp.smooth.spec<-function(object,data,knots){
 
 #   if(!is.null(object$xt$lims)){
 
-      lims<-object$xt$lims
-      sq<-object$xt$sq
+      #lims<-object$xt$lims
+      #sq<-object$xt$sq
       oldS<-object$S[[1]] 
 
       k<-dim(object$S[[1]])[1]
@@ -24,7 +24,7 @@ smooth.construct.mdstp.smooth.spec<-function(object,data,knots){
 #         # set some limits
 #         a<- lims[i]
 #         b<- lims[i+1]
-a<-0; b<-1
+      a<-0; b<-1
 
        
          N<-1000
@@ -45,10 +45,9 @@ a<-0; b<-1
          fd[,(k-1):k]<-rep(0,k*2)
 
          # do something here to adjust lambda
-         fd<-fd*100000
-#         fd<-fd*sqrt(sq[i]^3)
-#         fd<-fd*sqrt(sq[i])
-#         fd<-fd*sqrt((1/sq)^3)
+         #fd<-fd*100000
+         #fd<-fd*sqrt((1/sq)^3)
+         fd<-fd*sqrt((1/sq)^3)
 
          # do the integration
          D<-t(fd)%*%fd
@@ -70,30 +69,11 @@ a<-0; b<-1
       cat("max diff=",max(abs(oldS-object$S[[1]])),"\n")
 #   }
 
-    class(object)<-"mdstp"
+    class(object)<-"mdstp.smooth"
    object
 }
 
-Predict.matrix.mdstp.smooth<-function(object,data)
-# prediction matrix method for a t.p.r.s. term 
-{ x<-array(0,0)
-   for (i in 1:object$dim){
-      xx <- data[[object$term[i]]]
-      xx <- xx - object$shift[i]
-      if (i==1) n <- length(xx) else
-      if (length(xx)!=n) stop("arguments of smooth not same dimension")
-      if (length(xx)<1) stop("no data to predict at")
-      x<-c(x,xx)
-   }
-   
-   by<-0;by.exists<-FALSE
-   
-   X<-matrix(0,n,object$bs.dim)
-   oo<-.C(C_predict_tprs,as.double(x),as.integer(object$dim),as.integer(n),as.integer(object$p.order),
-       as.integer(object$bs.dim),as.integer(object$null.space.dim),as.double(object$Xu),
-       as.integer(nrow(object$Xu)),as.double(object$UZ),as.double(by),as.integer(by.exists),X=as.double(X))
-   X<-matrix(oo$X,n,object$bs.dim)
-   
-   X
+# prediction matrix method
+Predict.matrix.mdstp.smooth<-function(object,data){
+   Predict.matrix.tprs.smooth(object,data)
 }
-
