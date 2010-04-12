f184bbe2578d688008151f97b469e09b1ae4461f
David Lawrence Miller <dill@138-38-175-98.wireless.bath.ac.uk>
2010-04-12 15:48:49 +0100

    ...

--- mds/lanczos/arpack.r
+++ mds/lanczos/arpack.r
@@ -3,7 +3,7 @@
 library(igraph)
 
 
-f <- function(x, extra) extra%*%t(t(x))
+f <- function(x, extra) as.vector(extra%*%t(t(x)))
 
-arpack(f,diag(3), options=list(n=2, nev=12,which="LM"), sym=TRUE)$values
+arpack(f,diag(3), options=list(n=3, nev=2,which="LM"), sym=TRUE)$vectors
 
---
+++ mds/lanczos/mgcv_lanc.R
@@ -0,0 +1,15 @@
+mgcv_lanc<-function(A){
+
+#int lanczos_spd(matrix *A, matrix *V, matrix *va,int m,int lm)
+
+
+# m is the number of eigenvalues/vectors required at high end. Let A be n by n.
+#   lm is number of eigenvalues/vectors that must converge at low end.
+#   if lm<0 then the m largest magnitude eigenvalue + corresponding eigenvectors
+#   are found.
+
+
+
+
+
+}
