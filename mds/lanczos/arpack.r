b7c1836cc4afca2516b8b6c2e51ad4c7e1cc4c1e
David Lawrence Miller <dill@138-38-175-98.wireless.bath.ac.uk>
2010-04-13 14:52:52 +0100

    lanczos seems to work

--- mds/lanczos/arpack.r
+++ mds/lanczos/arpack.r
@@ -2,8 +2,21 @@
 
 library(igraph)
 
-
 f <- function(x, extra) as.vector(extra%*%t(t(x)))
 
-arpack(f,diag(3), options=list(n=3, nev=2,which="LM"), sym=TRUE)$vectors
+A<-matrix(runif(1000000),1000,1000)
+
+A<-A+t(A)
+
+
+t1<-system.time(ap<-arpack(f,A, options=list(n=dim(A)[1], nev=2,which="LM",ncv=100), sym=TRUE))[3]
+
+t2<-system.time(ei<-eigen(A))[3]
+
+cat("ARPACK=",t1,"\n")
+cat("eigen=",t2,"\n")
+
+# check
+#(A-ei$values[1]*diag(10))%*%ei$vector[,1]
+#(A-ap$values[1]*diag(10))%*%ap$vector[,1]
 
