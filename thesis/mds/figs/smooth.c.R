efab94b45a7fd4d6210fddffc14810376e55dac7
David Lawrence Miller <daveATninepointeightoneDOTnet>
2010-06-01 12:34:12 +0100

    adjustment writeup stuff...

---
+++ thesis/mds/figs/smooth.c.R
@@ -0,0 +1,70 @@
+# thin plate spline with squash
+# taken from smooth.r in mgcv
+
+## The constructor for a tprs basis object with MDS modifications.
+smooth.construct.mdstp.smooth.spec<-function(object,data,knots){
+
+   # make the tprs object as usual
+   object<-smooth.construct.tp.smooth.spec(object,data,knots)
+
+   # recreate the S matrix
+   # use finite difference to find the second derivatives
+   eps<- (1e-15)^(1/4)
+
+   oldS<-object$S[[1]] 
+
+   k<-dim(object$S[[1]])[1]
+   S<-matrix(0,k,k)
+
+   # integration limits
+   #a<-0
+   #b<-1
+   a<-min(data$x)
+   b<-max(data$x)
+
+   N<-1000
+   
+   # make the candidate x values
+   xs<-a+(1:N -0.5)*(b-a)/N
+   
+   # auto ks based adjustment
+   #library(ks)
+   #dens.est<-kde(data$x,h=hpi(data$x),eval.points=xs)
+   #sq<-dens.est$estimate
+
+   # find the density
+   sq<-object$xt$dens/diff(xs)[1]
+
+   # second difference
+   fd<-(Predict.matrix(object,data.frame(x=xs+2*eps))-
+       2*Predict.matrix(object,data.frame(x=xs+eps))+
+       Predict.matrix(object,data.frame(x=xs)))/eps^2
+
+   # adjust the entries of S
+   fd<-fd*sqrt((1/sq)^3)
+
+   # do the integration
+   S<-t(fd)%*%fd
+   S<-((b-a)/N)*S
+   
+   # enforce symmetry (from smooth.construct.tp...)
+   S <- (S + t(S))/2
+
+   object$S[[1]]<-S
+
+   # zero the last two rows and cols
+   #object$S[[1]][(k-1):k,]<-rep(0,k*2)
+   #object$S[[1]][,(k-1):k]<-rep(0,k*2)
+   object$S[[1]][k,]<-rep(0,k)
+   object$S[[1]][,k]<-rep(0,k)
+
+#   cat("max diff=",max(abs(oldS-object$S[[1]])),"\n")
+
+   class(object)<-"mdstp.smooth"
+   object
+}
+
+# prediction matrix method
+Predict.matrix.mdstp.smooth<-function(object,data){
+   Predict.matrix.tprs.smooth(object,data)
+}
---
+++ thesis/mds/figs/tpintexp.R
@@ -0,0 +1,98 @@
+# integration experiment (thin plate version)
+library(mgcv)
+#library(ks)
+
+# code to do the squashing
+source("squash.R")
+# code for the thin plate adjustment
+source("smooth.c.R")
+# code for the p-spline adjustment
+#source("psmooth.c.R")
+
+n<-100 
+#n<-200 # changing to 200 shows the wiggles
+x<-seq(0,1,len=n)
+#y<-x^2
+y <- 0.2 * x^11 * (10 * (1 - x))^6 + 10 * (10*x)^3 * (1 - x)^10
+# x values for prediction 
+newdat<-data.frame(x=x) 
+
+k<-60
+method<-"GCV.Cp"
+#method<-"REML"
+basis<-"mdsps"
+basis<-"mdstp"
+
+noise<-rep(0,length(x))
+#noise<-rnorm(length(x))*0.1
+
+dat<-data.frame(x=x,y=y+noise)
+
+b<-gam(y~s(x,k=k),data=dat,method=method)
+
+# send to a pdf...
+pdf(file="1dadjust.pdf",height=4,width=5)
+
+par(mfrow=c(2,2),las=1,mar=c(3,3,0.5,0.5),cex.axis=0.5,cex.lab=0.7,mgp=c(1.5,0.75,0))
+plot(x=newdat$x,y=predict(b,newdat),main="",type="l",xlim=c(0,1),col="black",xlab="x",ylab="y")
+
+
+###### now do some squashing...
+lims<-c(0,0.4,0.6,0.8,1)
+sq<-c(1/0.09,1,1/20,1)
+
+#lims<-c(0,0.5,1)
+#sq<-c(1,1/2)
+
+# do the squashing
+x.m<-squash(x,lims,sq)
+dat.m<-data.frame(x=x.m,y=y+noise)
+
+newdat.m<-data.frame(x=x.m)
+
+b.s<-gam(y~s(x,k=k),data=dat.m,method=method)
+
+plot(x=x.m,y=y,main="",type="l",xlim=c(0,max(x.m)),col="black",xlab="x*",ylab="y")
+
+
+##### fixing...
+alim<-min(x)
+blim<-max(x)
+#alim<-0
+#blim<-1
+N<-1000
+xs<-alim+(1:N -0.5)*(blim-alim)/N
+
+
+dens<-c(rep(sq[1],sum(xs<=lims[2])),
+        rep(sq[2],sum(xs>lims[2] & xs<=lims[3])),
+        rep(sq[3],sum(xs>lims[3] & xs<=lims[4])),
+        rep(sq[4],sum(xs>lims[4] & xs<=lims[5])))
+
+b.fix<-gam(y~s(x,k=k,bs=basis,xt=list(dens=dens)),data=dat.m,method=method)
+
+
+#par(mfrow=c(2,1))
+#adjusted fit (green), data (black),\n unadjusted fit (blue)
+plot(x=newdat.m$x,y=predict(b.fix,newdat.m),main="",type="n",xlim=c(0,max(x.m)),xlab="x*",ylab="y")
+points(x.m,y,pch=19,cex=0.3)
+lines(x=newdat.m$x,y=predict(b.s,newdat.m),col="blue",lwd=2)
+lines(x=newdat.m$x,y=predict(b.fix,newdat.m),col="green",lwd=2)
+rug(newdat.m$x)
+
+
+# now unsquash the predicted values...
+
+#adjusted fit (green), data (black),\n unadjusted fit (blue)
+plot(x=newdat$x,y=predict(b,newdat),main="",type="n",xlim=c(0,1),xlab="x",ylab="y")
+points(x=x,y=y+noise,pch=19,cex=0.3)
+lines(x=newdat$x,y=predict(b,newdat),lwd=2,col="red")
+lines(x=newdat$x,y=predict(b.s,newdat.m),col="blue",lwd=2)
+lines(x=newdat$x,y=predict(b.fix,newdat.m),col="green",lwd=2)
+rug(newdat$x)
+
+
+
+
+
+dev.off()
