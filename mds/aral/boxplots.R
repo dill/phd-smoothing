7976005417353e875246af13837e4d13a4e0539a
David Lawrence Miller <dave@ninepointeightone.net>
2010-07-14 16:39:10 +0100

    2d stuff done

--- mds/aral/boxplots.R
+++ mds/aral/boxplots.R
@@ -5,11 +5,13 @@ pdf(file="big-aral-boxplot.pdf",width=6.5,height=8.25)
 # make the text look better for printout
 par(mfrow=c(3,1),cex.axis=0.65,las=1,mgp=c(2,0.75,0),mar=c(2,3,1,1))
 
+# model names
+mod.names<-c("tprs","mds+tp","mds+cr","mds3D","mds+adj","soap")
 
 for(sampsize in c(100,250,500)){
    # stick all of the results into one matrix
    mses<-matrix(NA,100,0)
-   cols<-c()
+   cols<-c("white")
 
    for(err.lev in c("0.95","0.75","0.5")){
    
@@ -19,22 +21,18 @@ for(sampsize in c(100,250,500)){
       mses<-cbind(mses,mse)
    
       # extra Wilcoxon test stuff
-      for(i in 1:5){
-         if(wilcox.test(mse[,i],mse[,6],paired=TRUE)$p.value<0.01){
+      for(i in 2:6){
+         if(wilcox.test(mse[,1],mse[,i],paired=TRUE)$p.value<0.01){
             cols<-c(cols,"grey")
          }else{
             cols<-c(cols,"white")
          }
+         cat("tprs vs. ",mod.names[i],wilcox.test(mse[,1],mse[,i],paired=TRUE)$p.value,"\n")
       }
-      cols<-c(cols,"white")
    }
    
    # log the results
    mses<-log(mses)
-   
-   # model names
-#   mod.names<-c("tprs","mds+tp","mds+cr","mds 3D","mds+adj","soap")
-   mod.names<-c("tprs","mds+tp","mds+cr","mds3D","mds+adj","soap")
    
    # do the plot
    boxplot(mses,main="",names=rep(mod.names,3),
@@ -43,7 +41,5 @@ for(sampsize in c(100,250,500)){
          xlab="",
          ylab="log(mean MSE per realisation)")
 }
-
-
 
 dev.off()
