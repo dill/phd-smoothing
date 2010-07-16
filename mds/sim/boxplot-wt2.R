8d4ec78b39fe6530e919a24d72a794a7377531b8
David Lawrence Miller <dave@ninepointeightone.net>
2010-07-16 11:25:59 +0100

    wt2 changes

--- mds/sim/boxplot-wt2.R
+++ mds/sim/boxplot-wt2.R
@@ -8,12 +8,32 @@ par(cex.axis=0.75,las=1,mgp=c(2,0.75,0),mar=c(2,3,1,1))
 # stick all of the results into one matrix
 mses<-matrix(NA,100,0)
 
+# model names
+mod.names<-c("tprs","mds+tp","mds+cr","mds 3D","mds+adj","soap")
+
 for(err.lev in c("0.35","0.9","1.55")){
 
    mse<-read.csv(paste("wt2-mse-250-",err.lev,".csv",sep=""))
    mse<-mse[,-1]
    mse<-mse[1:100,]
    mses<-cbind(mses,mse)
+
+#   cols<-c()
+#
+#   # extra Wilcoxon test stuff
+#   for(i in 1:5){
+#      pv<-wilcox.test(mse[,6],mse[,i],paired=TRUE)$p.value
+#      med<-median(mse[,i]-mse[,6])
+#      if(pv<0.01 & med>0){
+#         cols<-c(cols,"red")
+#      }else if(pv<0.01 & med<0){
+#         cols<-c(cols,"green")
+#      }else{
+#         cols<-c(cols,"white")
+#      }
+#      cat("soap vs. ",mod.names[i],pv,"\n")
+#   }
+#   cols<-c(cols,"white")
 
 }
 
@@ -26,12 +46,12 @@ for(err.lev in c("0.35","0.9","1.55")){
 # log the results
 mses<-log(mses)
 
-# model names
-mod.names<-c("tprs","mds+tp","mds+cr","mds 3D","mds+adj","soap")
 
 # do the plot
 boxplot(mses,main="",names=rep(mod.names,3),
       xlab="",
+#      col=cols,
+      medlwd=1,
       ylab="log(mean MSE per realisation)")
 
 
