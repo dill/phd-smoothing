be9c1d204db0489ea42b8a2a054b7b6a5b2ef594
David Lawrence Miller <dave@ninepointeightone.net>
2011-02-26 09:49:37 +0000

    ...

--- duchon/wt2-gcvml.R
+++ duchon/wt2-gcvml.R
@@ -107,6 +107,46 @@ save.image("gcvml.RData")
 
 
 ## plot something
+load("gcvml.RData")
+library(dillhandy)
+library(ggplot2)
+
+real.results<-as.data.frame(real.results)
+real.results[,1]<-as.numeric(as.character(real.results[,1]))
+real.results[,2]<-as.numeric(as.character(real.results[,2]))+2
+real.results[,4]<-as.numeric(as.character(real.results[,4]))
+
+ml.mse<-as.data.frame(pe(real.results,real.results[,3]=="ml.mse"))
+gcv.mse<-as.data.frame(pe(real.results,real.results[,3]=="gcv.mse"))
+ml.score<-as.data.frame(pe(real.results,real.results[,3]=="ml.score"))
+gcv.score<-as.data.frame(pe(real.results,real.results[,3]=="gcv.score"))
+
+names(ml.mse)<-names(gcv.mse)<-names(ml.score)<-names(gcv.score)<-c("sim","dim","name","score")
+
+
+p<-ggplot(ml.score)
+p<-p+geom_line(aes(x=dim,y=score,group=sim))
+p
+
+
+p<-ggplot(gcv.score)
+p<-p+geom_line(aes(x=dim,y=score,group=sim))
+p
+
+
+
+p<-ggplot(gcv.mse)
+p<-p+geom_boxplot(aes(factor(dim),score))
+#p<-p+geom_line(aes(x=dim,y=score,group=sim))
+p
+
+
+p<-ggplot(ml.mse)
+p<-p+geom_boxplot(aes(factor(dim),score))
+#p<-p+geom_line(aes(x=dim,y=score,group=sim))
+p
+
+
 
 
 
