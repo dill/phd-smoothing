30d557abeea57acaca7cbb1975c9b87393b4932d
David Lawrence Miller <dave@ninepointeightone.net>
2011-02-28 16:27:49 +0000

    re-run gcvml without slodges

--- duchon/wt2-gcvml.R
+++ duchon/wt2-gcvml.R
@@ -17,7 +17,8 @@ bnd <- read.csv("wt2-verts.csv",header=FALSE)
 names(bnd)<-c("x","y")
 
 ## Simulate some fitting data, inside boundary...
-gendata<-read.csv("wt2truth-new.csv",header=TRUE)
+#gendata<-read.csv("wt2truth-new.csv",header=TRUE) # new with more splodges
+gendata<-read.csv("wt2truth.csv",header=TRUE) # old 
 onoff<-gendata$inside==1
 
 gendata<-list(x=gendata$x[gendata$inside==1],
@@ -103,76 +104,76 @@ for(i in 1:n.runs){
 #plot(res,xlab="MDS dimension",ylab="GCV Score",pch=19,cex=0.3)
 #abline(h=min(res[,2]),col="red")
 
-#save.image("gcvml.RData")
+save.image("gcvml.RData")
 
 
 ## plot something
-load("gcvml.RData")
-library(dillhandy)
-library(ggplot2)
-
-real.results<-as.data.frame(real.results)
-real.results[,1]<-as.numeric(as.character(real.results[,1]))
-real.results[,2]<-as.numeric(as.character(real.results[,2]))+2
-real.results[,4]<-as.numeric(as.character(real.results[,4]))
-
-ml.mse<-as.data.frame(pe(real.results,real.results[,3]=="ml.mse"))
-gcv.mse<-as.data.frame(pe(real.results,real.results[,3]=="gcv.mse"))
-ml.score<-as.data.frame(pe(real.results,real.results[,3]=="ml.score"))
-gcv.score<-as.data.frame(pe(real.results,real.results[,3]=="gcv.score"))
-
-names(ml.mse)<-names(gcv.mse)<-names(ml.score)<-names(gcv.score)<-c("sim","dim","name","score")
-
-
-p<-ggplot(ml.score)
-p<-p+geom_boxplot(aes(factor(dim),score))
-#p<-p+geom_line(aes(x=dim,y=score,group=sim))
-p
-
-
-p<-ggplot(gcv.score)
-p<-p+geom_boxplot(aes(factor(dim),score))
-#p<-p+geom_line(aes(x=dim,y=score,group=sim))
-p
-
-
-
-p<-ggplot(gcv.mse)
-p<-p+geom_boxplot(aes(factor(dim),score))
-#p<-p+geom_line(aes(x=dim,y=score,group=sim))
-p
-
-
-p<-ggplot(ml.mse)
-p<-p+geom_boxplot(aes(factor(dim),score))
-#p<-p+geom_line(aes(x=dim,y=score,group=sim))
-p
-
-
-# popularity contest
-a<-rep(NA,60)
-for(i in 1:60){
-   a[i]<-gcv.score$dim[gcv.score$sim==i][which.min(gcv.score$score[gcv.score$sim==i])]
-}
-attr(which.max(table(a)),"names")
-
-a<-rep(NA,60)
-for(i in 1:60){
-   a[i]<-ml.score$dim[ml.score$sim==i][which.min(ml.score$score[ml.score$sim==i])]
-}
-attr(which.max(table(a)),"names")
-
-
-a<-rep(NA,60)
-for(i in 1:60){
-   a[i]<-gcv.mse$dim[gcv.mse$sim==i][which.min(gcv.mse$score[gcv.mse$sim==i])]
-}
-attr(which.max(table(a)),"names")
-
-a<-rep(NA,60)
-for(i in 1:60){
-   a[i]<-ml.mse$dim[ml.mse$sim==i][which.min(ml.mse$score[ml.mse$sim==i])]
-}
-attr(which.max(table(a)),"names")
+#load("gcvml.RData")
+#library(dillhandy)
+#library(ggplot2)
+#
+#real.results<-as.data.frame(real.results)
+#real.results[,1]<-as.numeric(as.character(real.results[,1]))
+#real.results[,2]<-as.numeric(as.character(real.results[,2]))+2
+#real.results[,4]<-as.numeric(as.character(real.results[,4]))
+#
+#ml.mse<-as.data.frame(pe(real.results,real.results[,3]=="ml.mse"))
+#gcv.mse<-as.data.frame(pe(real.results,real.results[,3]=="gcv.mse"))
+#ml.score<-as.data.frame(pe(real.results,real.results[,3]=="ml.score"))
+#gcv.score<-as.data.frame(pe(real.results,real.results[,3]=="gcv.score"))
+#
+#names(ml.mse)<-names(gcv.mse)<-names(ml.score)<-names(gcv.score)<-c("sim","dim","name","score")
+#
+#
+#p<-ggplot(ml.score)
+#p<-p+geom_boxplot(aes(factor(dim),score))
+##p<-p+geom_line(aes(x=dim,y=score,group=sim))
+#p
+#
+#
+#p<-ggplot(gcv.score)
+#p<-p+geom_boxplot(aes(factor(dim),score))
+##p<-p+geom_line(aes(x=dim,y=score,group=sim))
+#p
+#
+#
+#
+#p<-ggplot(gcv.mse)
+#p<-p+geom_boxplot(aes(factor(dim),score))
+##p<-p+geom_line(aes(x=dim,y=score,group=sim))
+#p
+#
+#
+#p<-ggplot(ml.mse)
+#p<-p+geom_boxplot(aes(factor(dim),score))
+##p<-p+geom_line(aes(x=dim,y=score,group=sim))
+#p
+#
+#
+## popularity contest
+#a<-rep(NA,60)
+#for(i in 1:60){
+#   a[i]<-gcv.score$dim[gcv.score$sim==i][which.min(gcv.score$score[gcv.score$sim==i])]
+#}
+#attr(which.max(table(a)),"names")
+#
+#a<-rep(NA,60)
+#for(i in 1:60){
+#   a[i]<-ml.score$dim[ml.score$sim==i][which.min(ml.score$score[ml.score$sim==i])]
+#}
+#attr(which.max(table(a)),"names")
+#
+#
+#a<-rep(NA,60)
+#for(i in 1:60){
+#   a[i]<-gcv.mse$dim[gcv.mse$sim==i][which.min(gcv.mse$score[gcv.mse$sim==i])]
+#}
+#attr(which.max(table(a)),"names")
+#
+#a<-rep(NA,60)
+#for(i in 1:60){
+#   a[i]<-ml.mse$dim[ml.mse$sim==i][which.min(ml.mse$score[ml.mse$sim==i])]
+#}
+#attr(which.max(table(a)),"names")
 
 
