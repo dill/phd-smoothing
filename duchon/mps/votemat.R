91d6fbd300a1d6ebeffc2219438a9422ef504598
David Lawrence Miller <dave@ninepointeightone.net>
2011-03-10 19:41:45 +0000

    ready for some sims?

--- duchon/mps/votemat.R
+++ duchon/mps/votemat.R
@@ -37,12 +37,12 @@ votedates<-strptime(votes$date,"%Y-%m-%d")
 votemat<-t(as.matrix(votes[,5:(dim(votes)[2]-1)]))
 attr(votemat,"dimnames")[[2]]<-votes$voteno # put the vote number in
 # need to recode:
-# missing: -9     => NA
+# missing: -9     => 0
 # tellaye: 1      => 1
 # aye: 2          => 1
-# both: 3         => NA
-# no: 4           => 0
-# tellno: 5       => 0
+# both: 3         => 0
+# no: 4           => -1
+# tellno: 5       => -1
 votemat[votemat==-9]<-0
 votemat[votemat==1]<-1
 votemat[votemat==2]<-1
@@ -66,73 +66,88 @@ names(mpparty)<-"lab"
 # take a sample, fit a model, predict back
 
 samp.size<-100
+sim.size<-250
 
-# create sample and prediction data sets
-samp.ind<-sample(1:dim(votemat)[1],samp.size)
-samp.dat<-votemat[samp.ind,]
-pred.dat<-votemat[-samp.ind,]
+library(foreach)
+library(doMC)
+registerDoMC()
+options(cores=6)
+
+result<-foreach(i=1:sim.size,.combine=rbind,.init=c()) %dopar% {
+
+   retvec<-rep(0,length(mpid))
+
+   # create sample and prediction data sets
+   samp.ind<-sample(1:dim(votemat)[1],samp.size)
+   samp.dat<-votemat[samp.ind,]
+   pred.dat<-votemat[-samp.ind,]
+   
+   
+   D.samp<-dist(samp.dat)
+   
+   mds.dim<-choose.mds.dim(D.samp,0.75)
+   
+   mds.obj<-cmdscale(D.samp,mds.dim,eig=TRUE,k=mds.dim,x.ret=TRUE)
+   samp.mds<-mds.obj$points
+   
+   samp.mds<-cbind(mpparty[samp.ind],samp.mds)
+   attr(samp.mds,"dimnames")[[2]]<-c("lab",letters[(26-(dim(samp.mds)[2]-2)):26])
+   attr(samp.mds,"dimnames")[[1]]<-mpid[samp.ind]
+   samp.mds<-as.data.frame(samp.mds)
+   
+   # model setup
+   m<-c(2,(dim(samp.mds)[2]-1)/2-1)
+   gam.options<-paste("bs='ds',k=100, m=c(",m[1],",",m[2],")",sep="")
+   
+   # find the prediction terms
+   pred.terms<-letters[(26-(dim(samp.mds)[2]-2)):26]
+   pred.terms<-paste(pred.terms,collapse=",")
+   
+   # create the gam formula
+   gam.formula<-paste("lab~s(",paste(pred.terms,collapse=","),",",gam.options,")")
+   gam.formula<-as.formula(gam.formula)
+   
+   # run the model
+   b<-gam(gam.formula,data=samp.mds,family=binomial(link="logit"))
+   
+   
+   # predictions
+   
+   # map the predictions
+   # using code from insert.mds
+   lambda.inverse<-diag(1/mds.obj$eig[1:dim(mds.obj$points)[2]])
+   new.dist<-as.matrix(dist(votemat))[samp.ind,]
+   new.dist<-new.dist[,-samp.ind]
+   S<- -1/2*mds.obj$x
+   d<- -(new.dist^2-diag(S))
+   pred.mds<-t(1/2*(lambda.inverse %*% t(mds.obj$points) %*% d))
+   attr(pred.mds,"dimnames")[[2]]<-letters[(26-(dim(samp.mds)[2]-2)):26]
+   
+   # predict back over _all_ MPs
+   pred.mds<-as.data.frame(rbind(mds.obj$points,pred.mds))
+   
+   pr<-predict(b,pred.mds,type="response")
+   
+   pr[pr<0.5]<-0
+   pr[pr>0.5]<-1
+   
+   # mse
+   party.pred<-c(mpparty[samp.ind],mpparty[-samp.ind])
+   ds.mse<-sum((pr-party.pred)^2)
+   
+   
+   wrong<-mpid[(t(t(pr))-party.pred)!=0]
+   wrong.ind<-match(wrong,lookup$mpid)
+   
+   # return vector, put 1s where we messed up
+   retvec[wrong.ind]<-1
 
 
-D.samp<-dist(samp.dat)
+   return(retvec)
 
-mds.dim<-9#choose.mds.dim(D.samp,0.75)
-
-mds.obj<-cmdscale(D.samp,mds.dim,eig=TRUE,k=mds.dim,x.ret=TRUE)
-samp.mds<-mds.obj$points
-
-samp.mds<-cbind(mpparty[samp.ind],samp.mds)
-attr(samp.mds,"dimnames")[[2]]<-c("lab",letters[(26-(dim(samp.mds)[2]-2)):26])
-attr(samp.mds,"dimnames")[[1]]<-mpid[samp.ind]
-samp.mds<-as.data.frame(samp.mds)
-
-# model setup
-m<-c(2,(dim(samp.mds)[2]-1)/2-1)
-gam.options<-paste("bs='ds',k=100, m=c(",m[1],",",m[2],")",sep="")
-
-# find the prediction terms
-pred.terms<-letters[(26-(dim(samp.mds)[2]-2)):26]
-pred.terms<-paste(pred.terms,collapse=",")
-
-# create the gam formula
-gam.formula<-paste("lab~s(",paste(pred.terms,collapse=","),",",gam.options,")")
-gam.formula<-as.formula(gam.formula)
-
-# run the model
-b<-gam(gam.formula,data=samp.mds,family=binomial(link="logit"))
-
-
-# predictions
-
-# map the predictions
-# using code from insert.mds
-lambda.inverse<-diag(1/mds.obj$eig[1:dim(mds.obj$points)[2]])
-new.dist<-as.matrix(dist(votemat))[samp.ind,]
-new.dist<-new.dist[,-samp.ind]
-S<- -1/2*mds.obj$x
-d<- -(new.dist^2-diag(S))
-pred.mds<-t(1/2*(lambda.inverse %*% t(mds.obj$points) %*% d))
-attr(pred.mds,"dimnames")[[2]]<-letters[(26-(dim(samp.mds)[2]-2)):26]
-
-# predict back over _all_ MPs
-pred.mds<-as.data.frame(rbind(mds.obj$points,pred.mds))
-
-pr<-predict(b,pred.mds,type="response")
-
-pr[pr<0.5]<-0
-pr[pr>0.5]<-1
-
-# mse
-party.pred<-c(mpparty[samp.ind],mpparty[-samp.ind])
-ds.mse<-sum((pr-party.pred)^2)
-
-
-wrong<-mpid[(t(t(pr))-party.pred)!=0]
-wrong.ind<-match(wrong,lookup$mpid)
-
-wrong.names<-paste(lookup$firstname[wrong.ind],lookup$surname[wrong.ind])
-
-cat(wrong.names,sep="\n")
-
+#   wrong.names<-paste(lookup$firstname[wrong.ind],lookup$surname[wrong.ind])  
+#   cat(wrong.names,sep="\n")
+}
 
 #######################################################
 ## how does the LASSO do?
@@ -154,4 +169,11 @@ cat(wrong.names,sep="\n")
 #
 #
 #cat("LASSO MSE=",la.mse,"\n")
-cat("Duchon MSE=",ds.mse,"\n")
+#cat("Duchon MSE=",ds.mse,"\n")
+
+
+
+# save time
+save.image("mps-results.RData")
+
+
