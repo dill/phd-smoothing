ec05ff80699f6dd8efa90b8d08eb8d2b8d969609
David Lawrence Miller <dave@ninepointeightone.net>
2011-04-19 14:50:51 +0100

    added new thesis chapter on generalised distance smoothing
    microarray counter example code
    
    MPs results analysis

--- duchon/dsim/sim.R
+++ duchon/dsim/sim.R
@@ -4,91 +4,111 @@ library(mvtnorm)
 library(mgcv)
 library(mdspack)
 
+#qdist<-function(x,method,diag,upper,root=T,...){
+#   x.sq<-x^2
+#   x.cs<-rowSums(x.sq)
+#   #X.cs<-matrix(rep(x.cs,nrow(x)),nrow(x),nrow(x))
+#
+#   # x_i*x_j (in a matrix sense)
+#   x.cr<-x%*%t(x)
+#   
+#   #res<-X.cs+t(X.cs)-2*x.cr
+#
+#   res<- -2*x.cr
+#
+#   res<-t(t(res+x.cs)+x.cs)
+#
+#   diag(res)<-0
+#
+#   if(root)
+#      res<-sqrt(res)
+#
+#   return(res)
+#}
+#
+#dist<-qdist
+
 # some global options
-n.sims<-2#100
+n.sims<-1#0#100
 n.samp<-100
 mvn.mu<-c(0,0)
 mvn.sigma<-matrix(c(0.5,0,0,0.5),2,2)
 
-# Set the parameters for the alphas
-alpha1<-c(0.3,0.7)
-alpha2<-c(0.2,0.8)
-
 # prediction points
 grid.seq<-seq(-1,1,len=15)
 pred.points<-as.matrix(expand.grid(grid.seq,grid.seq,grid.seq))
-pred.z<-dmvnorm(cbind(pred.points[,1:2]%*%alpha1,pred.points[,3]),
+pred.z<-dmvnorm(pred.points[,1:2],
                   mvn.mu,mvn.sigma)
-pred.nonmds<-data.frame(x1=pred.points[,1:2]%*%alpha1,x2=pred.points[,3])
+pred.nonmds<-data.frame(x1=pred.points[,1],x2=pred.points[,2])
 
 bigletters<-as.vector(sapply(letters,paste,letters,sep=""))
 bl.len<-length(bigletters)
 
 mses<-c()
+edfs<-c()
 
 for(i in 1:n.sims){
 
    # Generate the dot xs from runif(-1,1)
-   xdots1<-cbind(runif(n.samp,-1,1),runif(n.samp,-1,1))
-   xdots2<-cbind(runif(n.samp,-1,1),runif(n.samp,-1,1))
-   
-   # Combine as x1 and x2 using the formulae
-   x1<-xdots1%*%alpha1
-   x2<-xdots2[,1]#%*%alpha2
+   xdots1<-runif(n.samp,-1,1)
+   xdots2<-runif(n.samp,-1,1)
+   x1<-xdots1
+   x2<-xdots2
    
    # Evaluate response z from MVN using x1 and x2 z=f(x1,x2)
    z<-dmvnorm(cbind(x1,x2),mvn.mu,mvn.sigma)
-   
-   # save the xdots
-   #xdots1.save<-xdots1
-   #xdots2.save<-xdots2
+   z<-z+rnorm(n.samp,0,0.05) # add error to the z
    
    # add error to the xdots
    xdots1<-xdots1#+cbind(rep(0,n.samp),0.7*runif(n.samp,-1,1))
    xdots2<-xdots2
+   # extra spurious columns
+#   noise.terms<-cbind(10*runif(n.samp,-1,1))
+   noise.terms<-cbind(10*rnorm(n.samp))
+   #noise.terms<-cbind(noise.terms,10*runif(n.samp,-1,1))
    
-   # can then check the SNR between the xdots
-   # and xdots with error added
-   # cor(xdots1,xdots1.save)^2
-   # cor(xdots2,xdots2.save)^2
-   
-   # Take the dot xs, put them in a matrix and calculate a distance matrix from them
+   # Take the dot xs, put them in a matrix
    #dotmat<-cbind(xdots1,xdots2,rnorm(n.samp)) # add in a column of noise
-   dotmat<-cbind(xdots1,xdots2[,1])
+   dotmat<-cbind(xdots1,xdots2,noise.terms)#,runif(n.samp,-10,10))
+   # calculate a distance matrix from them
    D2<-dist(dotmat)
    
-   
-   #par(mfrow=c(1,2))
-   
+   ### Fit some models
    # using the "true" parameters
    b<-gam(z~s(x1,x2,k=100),dat=data.frame(z=z,x1=x1,x2=x2))
-   ## plot that
-   #vis.gam(b,plot.type="contour",asp=1)
-   #points(x1,x2,pch=19,cex=0.7)
+   # Find the GCV optimal MDS projection -- smoothing over z using mdspack
+   b.mds<-gam.mds.fit(z,D2,NULL,k=100) # select the MDS dimension
+   #b.mds<-gam.mds.fit(z,D2,2,k=100) # just use 2d projection
 
+   ### Prediction
    fv.nonmds<-predict(b,pred.nonmds,type="response")
-
-   mses<-rbind(mses,c(sum((pred.z-fv.nonmds)^2),"nonmds")) 
-   
-   # Find the GCV optimal MDS projection while smoothing over z using mdspack
-   #b.mds<-gam.mds.fit(z,D2,NULL,k=100) # select the MDS dimension
-   b.mds<-gam.mds.fit(z,D2,2,k=100)
-   ## plot that
-   #vis.gam(b.mds$gam,plot.type="contour",asp=1)
-   #points(b.mds$samp.mds[,2:3],pch=19,cex=0.7)
-   
    # MDS pred points
    pred.mds<-insert.mds.generic(b.mds$mds.obj,pred.points,dotmat)
    pred.mds<-as.data.frame(pred.mds)
    attr(pred.mds,"names")<-bigletters[(length(bigletters)-
-                                      (dim(b.mds$samp.mds)[2]-2)):length(bigletters)]
+                               (dim(b.mds$samp.mds)[2]-2)):length(bigletters)]
    fv.mds<-predict(b.mds$gam,pred.mds,type="response")
+
+   #### Calculate MSEs
+   #mses<-rbind(mses,c(sum((pred.z-fv.nonmds)^2),sum((pred.z-fv.mds)^2)))
    
+   ### Plot all that
+   par(mfrow=c(1,2))
+   #vis.gam(b,plot.type="contour",asp=1)
+   #points(x1,x2,pch=19,cex=0.7)
+   #vis.gam(b.mds$gam,plot.type="contour",asp=1)
+   #points(b.mds$samp.mds[,2:3],pch=19,cex=0.7)
    
-   mses<-rbind(mses,c(sum((pred.z-fv.mds)^2),"mds")) 
-   
-   #
-   #Do some kind of cross validation
-   
+   image(grid.seq,grid.seq,matrix(fv.nonmds,length(grid.seq),length(grid.seq)),
+         asp=1,main="nonmds")
+   points(x1,x2,pch=19,cex=0.7)
+   image(grid.seq,grid.seq,matrix(fv.mds,length(grid.seq),length(grid.seq)),
+         asp=1,main="mds")
+   points(x1,x2,pch=19,cex=0.7)
+
+   # record the EDFs
+   edfs<-rbind(edfs,sum(b.mds$gam$edf))
+
 } 
    
+
--- duchon/mps/analyse-freesim.R
+++ duchon/mps/analyse-freesim.R
@@ -20,88 +20,98 @@ pushViewport(viewport(layout = Layout))
 
 
 # load the data and calculate a summary
-load("freesim.RData")
-labbin<-labbin[-del.rows,]
-lookup<-lookup[-del.rows,]
+bbrier<-c()
+bedf<-c()
+bmse<-c()
+bwrong.mat<-c()
 
-#
-samp.size<-300
-misclass<-read.csv(file=paste("freesim-misclass-",samp.size,".csv",sep=""))
+for(samp.size in c(200,300,400,500)){
+   load(paste("freesim-",samp.size,".RData",sep=""))
 
-model.names<-misclass[,687]
-wrong.mat<-misclass[,-c(1,687)]
+   # make a frame of Brier scores
+   brier<-brier[-1,]
+   melted.brier<-melt(brier)
+   bbrier<-rbind(bbrier,cbind(melted.brier,rep(samp.size,dim(melted.brier)[2])))
+   rm(brier)
 
-# now looking at the errors...
-res<-wrong.mat
-res<-matrix(as.numeric(as.matrix(res)),nrow(res),ncol(res))
-res<-res[,mpid]
+   # and for MSE
+   mse<-mse[-1,]
+   melted.mse<-melt(mse)
+   bmse<-rbind(bmse,cbind(melted.mse,rep(samp.size,dim(melted.mse)[2])))
+   rm(mse)
 
-#### per simulation results
-
-sim.res<-data.frame(mse=685-rowSums(res),
-                    model=model.names,
-                    sim=as.numeric(sapply(1:200,rep,5)))
-
-p<-ggplot(sim.res)
-p<-p+geom_histogram(aes(y=mse,sim,binwidth=1),stat="identity")
-p<-p+facet_wrap(~model,nrow=1)
-p<-p+labs(x="Simulation",y="Missclassifications")
-print(p,vp=subplot(1,1))
-
-
-#### per MP simulations
-
-ds.mps<-res[seq(1,200*5,5),]
-dsml.mps<-res[seq(2,200*5,5),]
-lasso.mps<-res[seq(3,200*5,5),]
-glmnet.mps<-res[seq(4,200*5,5),]
-glm.mps<-res[seq(5,200*5,5),]
-
-ds.mps<-data.frame(wrong=200-colSums(ds.mps),id=mpid,party=mpparty,model=rep("ds",676))
-dsml.mps<-data.frame(wrong=200-colSums(dsml.mps),id=mpid,party=mpparty,model=rep("dsml",676))
-lasso.mps<-data.frame(wrong=200-colSums(lasso.mps),id=mpid,party=mpparty,model=rep("lasso",676))
-glmnet.mps<-data.frame(wrong=200-colSums(glmnet.mps),id=mpid,party=mpparty,model=rep("glmnet",676))
-glm.mps<-data.frame(wrong=200-colSums(glm.mps),id=mpid,party=mpparty,model=rep("glm",676))
-
-mp.res<-as.data.frame(rbind(ds.mps,dsml.mps,glmnet.mps,lasso.mps,glm.mps))
-
-p<-ggplot(mp.res)
-p<-p+geom_histogram(aes(y=wrong,id,colour=party),stat="identity")
-p<-p+opts(legend.position="none")
-p<-p+facet_wrap(~model,nrow=1)
-p<-p+labs(x="MPs",y="Missclassifications")
-print(p,vp=subplot(2,1))
-
-for(mod in unique(model.names)){
-   cat(mod,mean(sim.res$mse[sim.res$model==mod]),"\n")
 }
 
-# what did the voting look like?
+# add some column names
+names(bbrier)<-c("method","score","samp.size")
+names(bmse)<-c("method","score","samp.size")
 
-# which were _very_ wrong
-#lasso.errors<-which(lasso.mps[,1]==200)
-#glmnet.errors<-which(glmnet.mps[,1]==200)
+# plot the Brier scores in boxplots
+p<-ggplot(bbrier)
+p<-p+geom_boxplot(aes(method,score))
+p<-p+facet_wrap(~samp.size)
+print(p,vp=subplot(1,1))
 
-##neworder.votemat<-rbind(votemat[lasso.errors,],votemat[-lasso.errors,])
-#neworder.votemat<-votemat
-#
-#new.names<-as.numeric(sub("mpid","",dimnames(neworder.votemat)[[1]]))
-#new.names[lasso.errors]<-new.names[lasso.errors]+1000
-#
-#dimnames(neworder.votemat)[[1]]<-new.names
-#
-#pvotemat<-melt(neworder.votemat)
-#names(pvotemat)<-c("mpid","voteid","vote")
-#pvotemat$mpid<-sub("mpid","",pvotemat$mpid)
-#
-#
-#
-#
-#quartz()
-#p<-ggplot(pvotemat)
-#p<-p+geom_tile(aes(y=factor(voteid),x=mpid,fill=vote))
-#print(p)
+# plot the MSE scores in boxplots
+p<-ggplot(bmse)
+p<-p+geom_boxplot(aes(method,score))
+p<-p+facet_wrap(~samp.size)
+print(p,vp=subplot(2,1))
 
 
 
+#labbin<-labbin[-del.rows,]
+#lookup<-lookup[-del.rows,]
+#
+##
+#samp.size<-300
+#misclass<-read.csv(file=paste("freesim-misclass-",samp.size,".csv",sep=""))
+#
+#model.names<-misclass[,687]
+#wrong.mat<-misclass[,-c(1,687)]
+#
+## now looking at the errors...
+#res<-wrong.mat
+#res<-matrix(as.numeric(as.matrix(res)),nrow(res),ncol(res))
+#res<-res[,mpid]
+#
+##### per simulation results
+#
+#sim.res<-data.frame(mse=685-rowSums(res),
+#                    model=model.names,
+#                    sim=as.numeric(sapply(1:200,rep,5)))
+#
+#p<-ggplot(sim.res)
+#p<-p+geom_histogram(aes(y=mse,sim,binwidth=1),stat="identity")
+#p<-p+facet_wrap(~model,nrow=1)
+#p<-p+labs(x="Simulation",y="Missclassifications")
+#print(p,vp=subplot(1,1))
+#
+#
+##### per MP simulations
+#
+#ds.mps<-res[seq(1,200*5,5),]
+#dsml.mps<-res[seq(2,200*5,5),]
+#lasso.mps<-res[seq(3,200*5,5),]
+#glmnet.mps<-res[seq(4,200*5,5),]
+#glm.mps<-res[seq(5,200*5,5),]
+#
+#ds.mps<-data.frame(wrong=200-colSums(ds.mps),id=mpid,party=mpparty,model=rep("ds",676))
+#dsml.mps<-data.frame(wrong=200-colSums(dsml.mps),id=mpid,party=mpparty,model=rep("dsml",676))
+#lasso.mps<-data.frame(wrong=200-colSums(lasso.mps),id=mpid,party=mpparty,model=rep("lasso",676))
+#glmnet.mps<-data.frame(wrong=200-colSums(glmnet.mps),id=mpid,party=mpparty,model=rep("glmnet",676))
+#glm.mps<-data.frame(wrong=200-colSums(glm.mps),id=mpid,party=mpparty,model=rep("glm",676))
+#
+#mp.res<-as.data.frame(rbind(ds.mps,dsml.mps,glmnet.mps,lasso.mps,glm.mps))
+#
+#p<-ggplot(mp.res)
+#p<-p+geom_histogram(aes(y=wrong,id,colour=party),stat="identity")
+#p<-p+opts(legend.position="none")
+#p<-p+facet_wrap(~model,nrow=1)
+#p<-p+labs(x="MPs",y="Missclassifications")
+#print(p,vp=subplot(2,1))
+#
+#for(mod in unique(model.names)){
+#   cat(mod,mean(sim.res$mse[sim.res$model==mod]),"\n")
+#}
 
