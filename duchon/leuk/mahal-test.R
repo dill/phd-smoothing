9eb291c6d2a9e5d42a45fe4c6928b36e6743d74c
David Lawrence Miller <dave@ninepointeightone.net>
2011-06-04 07:59:35 +0100

    quick mahalanobis done!

--- duchon/leuk/mahal-test.R
+++ duchon/leuk/mahal-test.R
@@ -2,14 +2,40 @@
 
 
 library(mdspack)
+library(MASS)
 
-x<-matrix(rnorm(100000,sd=100),100,1000)
+x<-c()
+for(i in 1:10){
+   x<-cbind(x,rnorm(12,sd=10*runif(1),mean=i))
+}
 
-mah1<-mahalanobis(x,x[1,])
 
+xc<-t(sweep(x,2,colMeans(x)))
+n<-nrow(x)
+
+# check that the covariance is calculated the same way
+#covd<-cov(x)-xc%*%t(xc)/(n-1) # TICK
+
+xc.svd<-svd(xc)
+nz<-xc.svd$d>sqrt(.Machine$double.eps)*xc.svd$d[1]
+
+# cov(x) is the same as it's SVD version
+#covsvdcheck<-cov(x)-
+#         (xc.svd$u[,nz]%*%diag(xc.svd$d[nz])^2%*%t(xc.svd$u[,nz]))/(n-1) # TICK
+
+# check that the inverses are the same
+
+cov.inv<-xc.svd$u[,nz]%*%(((1/xc.svd$d[nz])^2)*t(xc.svd$u[,nz]))*(n-1)
+#cov.inv<-xc.svd$u[,nz]%*%((((n-1)/xc.svd$d[nz])^2)*t(xc.svd$u[,nz]))
+invd<-ginv(cov(x))-cov.inv
+
+
+
+
+
+mah1<-stats::mahalanobis(x,x[1,],ginv(cov(x)),inverted=TRUE)
 source("ginv-leuk.R")
-
-mah2<-mahalanobis(x,x[1,])
+mah2<-mahal2(x,x[1,])
 
 
 
@@ -19,4 +45,6 @@ mah2<-mahalanobis(x,x[1,])
 #leuk.t<-leuk.num[,c(1:ncol(leuk.num))[!ind]]
 #
 #
-#system.time(mah2<-mahalanobis(leuk.t,leuk.t[1,]))
+##system.time(mah2<-mahalanobis(leuk.t,leuk.t[1,]))
+#source("ginv-leuk.R")
+#tt<-system.time(mah3<-mahalanobis(leuk.t,leuk.t[1,]))
