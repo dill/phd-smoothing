b7b6d929b203b283dd43512a073da39582a563ae
David Lawrence Miller <dave@ninepointeightone.net>
2011-06-13 12:00:11 +0100

    conf-sim -> lcpu

---
+++ duchon/leuk/insert.mds.generic.R
@@ -0,0 +1,32 @@
+# generic MDS insertion routine
+insert.mds.generic<-function(mds.obj,new.points,old.points,dist.metric="euclidean"){
+
+   big.points<-rbind(old.points,new.points)
+   ind<-1:nrow(old.points)
+   ind2<-(nrow(old.points)+1):nrow(big.points)
+
+   lambda.inverse<-diag(1/mds.obj$eig[1:dim(mds.obj$points)[2]])
+
+   if(dist.metric=="mahalanobis"){
+#      new.dist.start<-mahalanobis(big.points, big.points[1,])
+#      new.dist<-apply(big.points,1,mahalanobis,x=big.points,
+#                      cov=attr(new.dist.start,"cov.inv"))[ind,]
+
+      cov.mat<-try(solve(cov(big.points)))
+      if(class(cov.mat)=="try-error"){
+         library(MASS)
+         cov.mat<-ginv(cov(big.points))
+      }
+      new.dist<-apply(big.points,1,mahalanobis,x=big.points,
+                      cov=cov.mat,inverted=TRUE)[ind,]
+   }else{ 
+      new.dist<-as.matrix(dist(big.points,method=dist.metric,diag=T,upper=T))[ind,]
+   }
+
+   new.dist<-new.dist[,ind2]
+   S<- -1/2*mds.obj$x
+   d<- -(new.dist^2-diag(S))
+   mds.points<-t(1/2*(lambda.inverse %*% t(mds.obj$points) %*% d))
+
+   return(mds.points)
+}
