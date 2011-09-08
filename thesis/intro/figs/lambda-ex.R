ecb32213bd4a671c9707e1da75b355aaaa114d82
David Lawrence Miller <dave@ninepointeightone.net>
2011-09-08 15:31:09 +0100

    paranoia

--- thesis/intro/figs/lambda-ex.R
+++ thesis/intro/figs/lambda-ex.R
@@ -6,11 +6,8 @@ set.seed(12)
 
 # generate some data
 dat <- gamSim(1,n=100,dist="normal",scale=2)
-
 dat$y<-dat$y-(dat$f1+dat$f0+dat$f3)
-
-true<-data.frame(x=sort(dat$x2),y=dat$f2[order(dat$x2)]-5)
-
+true<-data.frame(x=sort(dat$x2),y=dat$f2[order(dat$x2)])#-5)
 
 pdf(file="lambda-ex.pdf",height=3,width=6.5)
 
@@ -19,19 +16,18 @@ par(mfrow=c(1,3),las=1,mgp=c(2,1,0))
 # optimal
 b<-gam(y~s(x2,k=100),data=dat)
 plot(b,se=FALSE,rug=FALSE,residuals=TRUE,ylab=expression(hat(f)(x)),xlab="x",ylim=c(-9,12))
-lines(true$x,true$y,type="l",col="blue")
+lines(true$x,true$y-coef(b)[1],type="l",col="blue")
 
 
 # lambda=0
 b.0<-gam(y~s(x2,k=100),data=dat,sp=0)
 plot(b.0,se=FALSE,rug=FALSE,residuals=TRUE,ylab=expression(hat(f)(x)),xlab="x",ylim=c(-9,12))
-lines(true$x,true$y,type="l",col="blue")
+lines(true$x,true$y-coef(b.0)[1],type="l",col="blue")
 
 
 # lambda=infinity
 b.inf<-gam(y~s(x2,k=100),data=dat,sp=1e10)
 plot(b.inf,se=FALSE,rug=FALSE,residuals=TRUE,ylab=expression(hat(f)(x)),xlab="x",ylim=c(-9,12))
-lines(true$x,true$y,type="l",col="blue")
-
+lines(true$x,true$y-coef(b.inf)[1],type="l",col="blue")
 
 dev.off()
