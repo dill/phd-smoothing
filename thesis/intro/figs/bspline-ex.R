b0642a930b7b4721cf5bd62abccd73ba1f8f72c5
David Lawrence Miller <dave@ninepointeightone.net>
2011-09-08 15:42:56 +0100

    paranoia

--- thesis/intro/figs/bspline-ex.R
+++ thesis/intro/figs/bspline-ex.R
@@ -4,20 +4,27 @@ library(mgcv)
 library(splines)
 
 
+pdf(file="bspline-ex.pdf",width=10.4,height=3.877)
+
 par(mfrow=c(1,3),las=1,mgp=c(2,1,0))
 
+x<-seq(-1,2,len=400)
+knots<-seq(-0.5,1.5,len=20)
 
 for(bs.dim in c(1,2,3)){
    # create some degree 1 basis functions
-   bs.basis<-bs(seq(0,1,len=100),knots=seq(0,1,len=10),degree=bs.dim)
+   bs.basis<-bs(x,knots=knots,degree=bs.dim)
    
    # plot them
    
    
-   plot(x=seq(0,1,len=100),y=bs.basis[,1],type="l",
+   plot(x=x,y=bs.basis[,1],type="l",
+         xlim=c(0,1),ylim=c(0,1),
          xlab="x",ylab=bquote(B[j]^.(bs.dim)(x)),asp=1)
    
    for(i in 2:ncol(bs.basis)){
-      lines(x=seq(0,1,len=100),y=bs.basis[,i],lty=i)
+      lines(x=x,y=bs.basis[,i],lty=i)
    }
 }
+
+dev.off()
