338ed205b379d7dd2fc259f98f862c324cd40ade
David Lawrence Miller <dave@ninepointeightone.net>
2011-09-12 10:51:25 +0100

    paranoia

---
+++ thesis/intro/figs/tprsex.R
@@ -0,0 +1,38 @@
+# thin plate example
+
+pdf(file="tprsex.pdf",width=5,height=5)
+
+
+# plotting options
+par(mfrow=c(2,2),mar=c(2,2.2,1.8,1.5),las=1)
+
+
+# grid setup 
+n<-100
+x1<-seq(0,1,len=n)
+x.grid<-expand.grid(x1,x1)
+
+# constant
+image(matrix(1,n,n),col=heat.colors(1000),xlab=expression(x[1]),
+                                    ylab=expression(x[2]),asp=1)
+
+# phi_2=x_1
+image(matrix(x1,n,n),col=heat.colors(1000),xlab=expression(x[1]),
+                                    ylab=expression(x[2]),asp=1)
+
+image(t(matrix(x1,n,n)),col=heat.colors(1000),xlab=expression(x[1]),
+                                    ylab=expression(x[2]),asp=1)
+
+# radial basis function
+rbf<-function(x1,x2){
+
+   r<-sqrt(rowSums((x1-x2)^2))
+
+   1/(8*pi) * r^2 * log(r)
+}
+res<-rbf(x.grid,c(0.5,0.5))
+img.mat<-matrix(res,n,n)
+image(img.mat,col=heat.colors(1000),xlab=expression(x[1]),
+                                    ylab=expression(x[2]),asp=1)
+
+dev.off()
